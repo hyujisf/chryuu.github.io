bca9e49c9474f18aa849217016feea9a84141fca
yuu27q <canqalloid.me@gmail.com>
2022-04-13 05:51:06 +0700

    6b9cde5

---
+++ _nuxt/12101bd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{318:function(e,n,t){"use strict";t.r(n);var o={name:"AboutPage",layout:"home"},l=t(11),component=Object(l.a)(o,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",{},[e._v("About")])}),[],!1,null,null,null);n.default=component.exports}}]);
